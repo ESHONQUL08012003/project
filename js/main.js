7bf6fd3844a3b0aff71657d6eb800200b43d9a79
ESHONQUL08012003 <abdulazizoveshonqul66@gmail.com>
2022-11-12 17:03:18 +0500

    edit dashboard

--- js/main.js
+++ js/main.js
@@ -1,55 +1,132 @@
-let data = "http://localhost:5001/useres"
+const data = "http://localhost:5500/useres";
 
-  fetch(data)
-  .then((respons) => respons.json())
-  .then((result) => rendereData(result));
-
-function rendereData(array = []) {
-  array.forEach((e) => {
+// renderUser
+const RenderUser = (user) => {
+  let userFragment = document.createDocumentFragment();
+  user.forEach((e) => {
     let newUser = createElement(
       "tr",
       "bg-light",
       `
-      <td>${e.id}</td>
-		 <td>${e.user_name}</td>
-		 <td>${e.score}</td>
-		 <td>
-			<button class="btn btn-primary" data-edit=${e.id}>Edit</button>
-		 </td>
-		 <td>
-			<button class="btn btn-danger data-delet="${e.id}">Delet</button>
-		 </td>
-        `
+        <td>${e.id}</td>
+      	<td>${e.user_name}</td>
+      	<td>${e.score}</td>
+      	<td>
+      	<button class="btn btn-primary edid-btn" data-mdb-toggle="modal" data-mdb-target="#exampleModal" data-edit=${e.id}>Edit</button>
+      	</td>
+      	<td>
+      	<button class="btn btn-danger del-btn" data-delet="${e.id}">Delet</button>
+      	</td>
+      `
     );
-
-    $("tbody").append(newUser);
+    userFragment.appendChild(newUser);
   });
-}
+  $("tbody").appendChild(userFragment);
+};
 
-function addData() {
-  let userNameValue = $(".userName").value.trim();
-  let retingValue = $(".userReting").value.trim();
+const getUser = async () => {
+  try {
+    const respons = await fetch(data);
+    const result = await respons.json();
 
-  const user = {
-    user_name: userNameValue,
-    score: retingValue
+    RenderUser(result);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getUser();
+
+$("#form").addEventListener("submit", (e) => {
+  addUser();
+});
+
+// renderUser end
+
+// addUser
+
+const addUser = async () => {
+  const userValue = $("#form1").value.trim();
+  const retingValue = $("#form2").value.trim();
+
+  const person = {
+    user_name: userValue,
+    score: retingValue,
   };
-  if (userNameValue.length === 0 || retingValue.length === 0) {
-    alert("ILTIMOS TO'LDIRING");
+
+  if (userValue.length === 0 || retingValue.length === 0) {
+    alert("malumot To'ldiring");
   } else {
-    fetch(data, {
+    const respond = await fetch(data, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(user)
-    })
-      .then((respons) => respons.json())
-      .then((result) => rendereData(result));
+      body: JSON.stringify(person),
+    });
+    const result = await respond.json();
+    getUser(result);
   }
-}
+};
 
-$("#form").addEventListener("submit", (e) => {
-  e.preventDefault();
-  addData();
+// addUser end
+
+// deletUse
+
+$("tbody").addEventListener("click", (e) => {
+  if (e.target.classList.contains("del-btn")) {
+    let deletId = e.target.getAttribute("data-delet");
+    delUser(deletId);
+  }
 });
+
+const delUser = (userId) => {
+  fetch(`http://localhost:5500/useres/${userId}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({}),
+  });
+};
+
+// deletUse end
+
+$("tbody").addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn-primary")) {
+    let id = e.target.getAttribute("data-edit");
+    localStorage.setItem("editUser", id);
+
+    fetch(`http://localhost:5500/useres/${id}`)
+      .then((res) => res.json())
+      .then((result) => setValue(result))
+      .catch((err) => console.log(err));
+  }
+});
+
+const updateUser = () => {
+  let id = localStorage.getItem("editUser");
+
+  let newUser = $("#typeText").value.trim();
+  let newScore = $("#typeNumber").value.trim();
+
+  fetch(`http://localhost:5500/useres${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      user_name: newUser,
+      score: newScore,
+    }),
+  });
+};
+
+$(".changesData").addEventListener("submit", () => {
+  updateUser();
+});
+
+function setValue(data) {
+  $("#typeText").value = data.user_name;
+  $("#typeNumber").value = data.score;
+}
